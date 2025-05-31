50f38918aeb48c3f01c890c671f694b41a573e66
Md. Shahidul Islam <shanto57575@gmail.com>
2025-05-31 19:05:48 +0600

    added contact page

--- src/components/HomePageComponents/Impact.jsx
+++ src/components/HomePageComponents/Impact.jsx
@@ -2,6 +2,7 @@ import { IoIosPeople } from "react-icons/io";
 import { TiTick } from "react-icons/ti";
 import { FaLaptopCode } from "react-icons/fa";
 import { FaClock } from "react-icons/fa6";
+import { Zoom } from "react-awesome-reveal";
 
 const Impact = () => {
 	return (
@@ -19,39 +20,42 @@ const Impact = () => {
 				</p>
 			</div>
 
-			<div className="grid grid-cols-2 md:grid-cols-4 gap-10 mt-12 lg:px-16">
-				<div className="flex flex-col items-center min-w-[120px]">
-					<IoIosPeople size={40} className="text-[#5E35B1]" />
-					<div className="text-center mt-5">
-						<p className="font-bold text-lg font-sans mb-2">1000+</p>
-						<p className="text-sm">Happy Clients</p>
+			<Zoom>
+				{" "}
+				<div className="grid grid-cols-2 md:grid-cols-4 gap-10 mt-12 lg:px-16">
+					<div className="flex flex-col items-center min-w-[120px]">
+						<IoIosPeople size={40} className="text-[#5E35B1]" />
+						<div className="text-center mt-5">
+							<p className="font-bold text-lg font-sans mb-2">1000+</p>
+							<p className="text-sm">Happy Clients</p>
+						</div>
 					</div>
-				</div>
 
-				<div className="flex flex-col items-center min-w-[120px]">
-					<FaLaptopCode size={40} className="text-[#5E35B1]" />
-					<div className="text-center mt-5">
-						<p className="font-bold text-lg font-sans mb-2">100+</p>
-						<p className="text-sm">Finished Projects</p>
+					<div className="flex flex-col items-center min-w-[120px]">
+						<FaLaptopCode size={40} className="text-[#5E35B1]" />
+						<div className="text-center mt-5">
+							<p className="font-bold text-lg font-sans mb-2">100+</p>
+							<p className="text-sm">Finished Projects</p>
+						</div>
 					</div>
-				</div>
 
-				<div className="flex flex-col items-center min-w-[120px]">
-					<TiTick size={40} className="text-[#5E35B1]" />
-					<div className="text-center mt-5">
-						<p className="font-bold text-lg font-sans mb-2">99%</p>
-						<p className="text-sm">Satisfaction Rate</p>
+					<div className="flex flex-col items-center min-w-[120px]">
+						<TiTick size={40} className="text-[#5E35B1]" />
+						<div className="text-center mt-5">
+							<p className="font-bold text-lg font-sans mb-2">99%</p>
+							<p className="text-sm">Satisfaction Rate</p>
+						</div>
 					</div>
-				</div>
 
-				<div className="flex flex-col items-center min-w-[120px]">
-					<FaClock size={35} className="text-[#5E35B1]" />
-					<div className="text-center mt-5">
-						<p className="font-bold text-lg font-sans mb-2">10+</p>
-						<p className="text-sm">Years of Experience</p>
-					</div>
+						<div className="flex flex-col items-center min-w-[120px]">
+							<FaClock size={35} className="text-[#5E35B1]" />
+							<div className="text-center mt-5">
+								<p className="font-bold text-lg font-sans mb-2">10+</p>
+								<p className="text-sm">Years of Experience</p>
+							</div>
+						</div>
 				</div>
-			</div>
+			</Zoom>
 		</div>
 	);
 };
