32e02e344e12526b967541e2f76611af5f943a64
Md. Shahidul Islam <shanto57575@gmail.com>
2025-05-31 13:59:46 +0600

    added banner including two more section in the home page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
 	return (
-		<div className="max-w-7xl mx-auto">
+		<div className="max-w-7xl mx-auto font-roboto">
 			<Navbar />
 
 			<div className="min-h-screen px-4">
