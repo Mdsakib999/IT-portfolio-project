bbe3672982287edcc6d61c3eae09537ee9665e00
LaZyFee <rhr277@gmail.com>
2025-05-31 14:21:14 +0600

    service page processing

---
+++ src/pages/Service.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ServiceBanner } from "../components/ServicePageComponents/ServiceBanner";
+import { HireBest } from "../components/ServicePageComponents/HireBest";
+
+export const Service = () => {
+  return (
+    <div className="">
+      <ServiceBanner />
+      <HireBest />
+    </div>
+  );
+};
